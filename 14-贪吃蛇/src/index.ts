f4e59ca4efcc9b16454e4d8d6c6c1cecdee38a17
BarryDong <286084845@qq.com>
2022-05-30 15:27:04 +0800

    贪吃蛇-完成Food类

--- 14-贪吃蛇/src/index.ts
+++ 14-贪吃蛇/src/index.ts
@@ -1,3 +1,31 @@
 import './index.less'
 
-console.log(123)
+class Food {
+    element: HTMLElement
+    constructor() {
+        this.element = document.querySelector('#food')!
+    }
+    // 获取食物的坐标
+    get position() {
+        return {
+            x: this.element.offsetLeft,
+            y: this.element.offsetTop
+        }
+    }
+    // 改变食物的坐标（坐标随机）
+    changePosition() {
+        const width = (this.element.parentElement!.offsetWidth - 14) / 10
+        const height = (this.element.parentElement!.offsetHeight - 14) / 10
+        const left = Math.round(Math.random() * width) * 10
+        const top = Math.round(Math.random() * height) * 10
+        this.element.style.left = `${left}px`
+        this.element.style.top = `${top}px`
+    }
+}
+
+// const food = new Food()
+// console.log(food.position)
+
+// setTimeout(() => {
+//     food.changePosition()
+// }, 1000)
